18f2a75238e6b09a003c9b69df0110af283c2eea
Chris Fryzek <cfryzek2@huskers.unl.edu>
2021-08-26 16:17:24 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Chris Fryzrk
+ *         cfryzek2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chris Fryzek\n");
+
+  return 0;
+}
